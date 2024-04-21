91f001739e3740f2526cc4aabf86ac116e1dc8a8
SBK850 <160433022+SBK850@users.noreply.github.com>
2024-04-22 00:41:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const csrf = require('csurf');
-const { URL } = require('url');
 
 const app = express();
 const csrfProtection = csrf({ cookie: true });
-const parseForm = express.urlencoded({ extended: false });
 
 app.use(cors({
     origin: 'https://main.dc5bz0dk3svjz.amplifyapp.com',
@@ -15,18 +13,18 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-
 app.use(express.json());
-
 app.use(express.static('public'));
 
-app.get('/get-csrf-token', csrfProtection, (req, res) => {
+// Corrected endpoint
+app.get('/api/get-csrf-token', csrfProtection, (req, res) => {
     res.json({ csrfToken: req.csrfToken() });
 });
 
-app.post('/api/process-url', parseForm, csrfProtection, (req, res) => {
+app.post('/api/process-url', csrfProtection, (req, res) => {
     try {
         const { url } = req.body;
+        // Ensure the URL is valid
         new URL(url);
         console.log('Received valid URL:', url);
         res.status(200).json({ message: 'URL processed successfully' });
